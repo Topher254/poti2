f66d03e6c2e74c4a8f50c34e53906359456d2c3c
Topher254 <raphaelsarota@gmail.com>
2026-03-28 01:46:41 +0300

    ready to deploy 1

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://potibackend.onrender.com/api' });
+// const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
 export default API;
