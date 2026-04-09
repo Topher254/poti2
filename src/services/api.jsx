340854a9170b0a810740888b32a5409048c97905
Topher254 <raphaelsarota@gmail.com>
2026-04-09 19:30:40 +0300

    reday to deploy 6. again  twice

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://potibackend.onrender.com/api' });
+const API = axios.create({ baseURL: 'https://potibackend-1.onrender.com' });
 // const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
 export default API;
